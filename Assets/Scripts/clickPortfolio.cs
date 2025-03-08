0b78504b4a187fb6f20fb3d27da5db50fcd564f6
jonelvasquez <jonelevasquez.d@gmail.com>
2025-03-08 23:28:27 +0100

    ultimo

--- Assets/Scripts/clickPortfolio.cs
+++ Assets/Scripts/clickPortfolio.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class clickPortfolio : MonoBehaviour
 {
-    Canvas canvas;
-    void Update()
+    [SerializeField] GameObject canvas;
+    SpriteMask mask;
+    [SerializeField] Sprite sprite;
+
+    private void Start()
     {
-        
+        mask = GetComponent<SpriteMask>();
     }
     private void OnMouseDown()
     {
-        
+        if (canvas != null)
+        {
+            canvas.SetActive(true);
+            print("print portfolio image");
+        }
+
     }
 }
